04aa6708b8107d34681d6f7c3ebea2b5920a6679
AriesUnderground <63938402+AriesUnderground@users.noreply.github.com>
2020-04-19 13:40:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ var tries=4
 function checkGuess() {
 	var inputVal=document.getElementById('guess').value
 	//alert(randnum +"   "+tries+"   "+inputVal)
-	if (inputVal != randnum && tries>0) {
-		//document.getElementById('lives').label="you have "+tries+" lives remaining"
+	if (tries > 0 ) {
+		if (inputVal != randnum) {
+			//document.getElementById('lives').label="you have "+tries+" lives remaining"
 		
-		if (tries > 1){
-			alert("you have "+tries+" lives remaining")
-		}else {
-			alert("you have "+tries+" life remaining")			
+			if (tries > 1){
+				alert("you have "+tries+" lives remaining")
+			}else {
+				alert("you have "+tries+" life remaining")			
+			}
+		
+			tries-=1
 		}
-		
-		tries-=1
-	}
-	else if (inputVal==randnum && tries>=0) {
-		alert('You Won!')
-		location.reload()
+		else if (inputVal==randnum) {
+			alert('You Won!')
+			location.reload()
+		}
 	}
 	else {
 		alert("You Lose")
